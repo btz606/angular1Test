1697874ff9b84742e72fffae20a611d64b153e5f
btz <btz606@163.com>
2018-11-12 20:31:42 +0800

    layDate练习

---
+++ allTestDemo/layDateTest/test.js
@@ -0,0 +1,12 @@
+var editMembers = {
+    dashBoard: {
+        "parentId": "000000000000000000",
+        "loginName": "13269421883",
+        "organizationId": "53305584107157536",
+        "type": 0,
+        "workGroupId": "5646565264"
+    }
+
+
+
+}
